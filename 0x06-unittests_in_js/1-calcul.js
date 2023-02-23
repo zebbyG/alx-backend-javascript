795fe6f8b060fe5df2e52f4ca6a88109756594e4
zebbyG <zebbynjau@gmail.com>
2023-02-23 10:44:39 +0300

    chanscw

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,17 +1,17 @@
 function calculateNumber(type, a, b) {
-        if (type === 'SUM') {
-            return Math.round(a) + Math.round(b);
-        }
-        if (type === 'SUBSTRACT') {
-            return Math.round(a) - Math.round(b);
-        }
-        if (type === 'DIVIDE') {
-            if  (Math.round(b) === 0) {
-                return 'Error';
-                }
-            return Math.round(a) / Math.round(b);
-        }
-        return 0;
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+  return 0;
 }
-console.log(calculateNumber('DIVIDE',1.4, 0))
+
 module.exports = calculateNumber;
